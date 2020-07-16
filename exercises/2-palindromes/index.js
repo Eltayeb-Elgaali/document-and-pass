1a8704c8ac6786e5db554f8121b8bbc88493d973
eltayeb <gaaliii@yahoo.com>
2020-07-16 20:10:39 +0200

    2-palindromes/index

--- exercises/2-palindromes/index.js
+++ exercises/2-palindromes/index.js
@@ -1,8 +1,21 @@
 /**
+ * This function determines whether a string is a palindrome.
+ * A palindrome is a word, phrase, number, or other sequence of characters which reads the same backward or forward.
+ * 
+ * @param {string} str - the string to be checked
+ * @returns {boolean} true if palindromes and false if not.
+ *
+ * @example
+ *
+ * const isStrPalindrome = palindromes('hah');
+ * console.log(isStrPalindrome); // true
  *
  */
-const palindromes = () => {
-
+const palindromes = (str) => {
+    var re = /[\W_]/g;
+  var lowRegStr = str.toLowerCase().replace(re, '');
+  var reverseStr = lowRegStr.split('').reverse().join(''); 
+  return reverseStr === lowRegStr;
 };
 
 module.exports = palindromes;
